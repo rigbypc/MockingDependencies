ad2886b931314526afc76f47e3b3d5cfc8af84b5
Peter C Rigby <rigbypc@gmail.com>
2018-01-24 15:37:40 -0500

    Mock the Storage using when and verify

--- MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
+++ MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
@@ -4,14 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import point.of.sale.FakeDisplay;
-import point.of.sale.FakeStorage;
-import point.of.sale.HashStorage;
-import point.of.sale.Sale;
+import point.of.sale.*;
+import static org.mockito.Mockito.*;
 
 public class TestSale {
 
 	@Test
+	public void testMockStorage() {
+		FakeDisplay fakeDisplay = new FakeDisplay();
+		//create the mock of Storage
+		Storage mockStorage = mock(Storage.class);
+		//stub barcode("123") to return Milk, 3.99
+		when(mockStorage.barcode("123")).thenReturn("Milk, 3.99");
+		
+		//call the class under test
+		Sale sale = new Sale(fakeDisplay, mockStorage);
+		//internally this will use the mock object
+		sale.scan("123");
+		//assert what was displayed
+		assertEquals("Milk, 3.99", fakeDisplay.getLastLine());
+		
+		//verify that barcode was called by the sale object
+		verify(mockStorage).barcode("123");
+		
+	}
+	
 	public void testFake() {
 		FakeDisplay fakeDisplay = new FakeDisplay();
 		FakeStorage fakeStorage = new FakeStorage();
@@ -21,7 +38,6 @@ public class TestSale {
 		
 	}
 	
-	@Test
 	public void testHashStorage() {
 		FakeDisplay fakeDisplay = new FakeDisplay();
 		
