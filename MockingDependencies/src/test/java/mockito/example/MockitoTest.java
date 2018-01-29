be38dba4b14bb7f9964a46b91574d025b8c4624f
Peter C Rigby <rigbypc@gmail.com>
2018-01-29 15:11:26 -0500

    The stack

--- MockingDependencies/src/test/java/mockito/example/MockitoTest.java
+++ MockingDependencies/src/test/java/mockito/example/MockitoTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Stack;
+
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
@@ -18,6 +20,21 @@ public class MockitoTest {
     when(mockList.get(1)).thenReturn("JCG");
     assertEquals("Mockito", mockList.get(0));
     assertEquals("JCG", mockList.get(1));
+  }
+  
+  @Test
+  public void testStack() {
+	  
+	  Stack<Integer> mockStack = mock(Stack.class);
+	  
+	  when(mockStack.pop()).thenReturn(3, 2, 1);
+	  
+	  System.out.println(mockStack.pop());
+	  System.out.println(mockStack.pop());
+	  System.out.println(mockStack.pop());
+	  System.out.println(mockStack.pop());
+	  //assertEquals(3, mockStack.pop().intValue());
+	  
   }
 }
 
