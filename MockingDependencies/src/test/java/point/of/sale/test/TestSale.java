342be1f47bee9fcada58133060d691aba18f8915
Peter C Rigby <rigbypc@gmail.com>
2018-01-29 15:11:44 -0500

    Test another item

--- MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
+++ MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
@@ -11,17 +11,21 @@ public class TestSale {
 
 	@Test
 	public void testMockStorage() {
+		//create the mock Display
 		Display mockDisplay = mock(Display.class);
 				
 		//create the mock of Storage
 		Storage mockStorage = mock(Storage.class);
+		
 		//stub barcode("123") to return Milk, 3.99
 		when(mockStorage.barcode("123")).thenReturn("Milk, 3.99");
+		when(mockStorage.barcode("124")).thenReturn("Eggs, 4.99");
 		
 		//call the class under test
 		Sale sale = new Sale(mockDisplay, mockStorage);
 		//internally this will use the mock object
 		sale.scan("123");
+		sale.scan("124");
 		
 		//verify display was called with "123"
 		verify(mockDisplay).showLine("123");
@@ -30,6 +34,9 @@ public class TestSale {
 		
 		//verify that Storage.barcode was called by the sale object
 		verify(mockStorage).barcode("123");
+		
+		verify(mockStorage).barcode("124");
+		verify(mockDisplay).showLine("Eggs, 4.99");
 		
 	}
 	
