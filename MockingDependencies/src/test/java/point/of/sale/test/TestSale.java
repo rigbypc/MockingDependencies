0ca3b12a096220077f9c4b8d15b6a9a6ac5bf48a
Peter C Rigby <rigbypc@gmail.com>
2018-01-29 15:16:52 -0500

    Check the order of calls

--- MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
+++ MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
@@ -3,6 +3,7 @@ package point.of.sale.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import point.of.sale.*;
 import static org.mockito.Mockito.*;
@@ -21,6 +22,8 @@ public class TestSale {
 		when(mockStorage.barcode("123")).thenReturn("Milk, 3.99");
 		when(mockStorage.barcode("124")).thenReturn("Eggs, 4.99");
 		
+		InOrder inOrder = inOrder(mockDisplay, mockStorage); 
+		
 		//call the class under test
 		Sale sale = new Sale(mockDisplay, mockStorage);
 		//internally this will use the mock object
@@ -35,8 +38,9 @@ public class TestSale {
 		//verify that Storage.barcode was called by the sale object
 		verify(mockStorage).barcode("123");
 		
-		verify(mockStorage).barcode("124");
-		verify(mockDisplay).showLine("Eggs, 4.99");
+		inOrder.verify(mockStorage).barcode("124");
+		inOrder.verify(mockDisplay).showLine("Eggs, 4.99");
+		
 		
 	}
 	
