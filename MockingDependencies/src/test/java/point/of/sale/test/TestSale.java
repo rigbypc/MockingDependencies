5dce98d14c0e274237afda69316e1aa9184e2945
Peter C Rigby <rigbypc@gmail.com>
2018-01-29 15:33:36 -0500

    Arg Captor

--- MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
+++ MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
@@ -3,6 +3,7 @@ package point.of.sale.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 
 import point.of.sale.*;
@@ -22,18 +23,27 @@ public class TestSale {
 		when(mockStorage.barcode("123")).thenReturn("Milk, 3.99");
 		when(mockStorage.barcode("124")).thenReturn("Eggs, 4.99");
 		
+		//Order
 		InOrder inOrder = inOrder(mockDisplay, mockStorage); 
 		
+		//ArgCaptor: Can capture a string
+		ArgumentCaptor<String> argCaptor = ArgumentCaptor.forClass(String.class); 
+				
 		//call the class under test
 		Sale sale = new Sale(mockDisplay, mockStorage);
 		//internally this will use the mock object
 		sale.scan("123");
-		sale.scan("124");
+		//checks that the call was made, but also tells what the parameter value was
+		verify(mockStorage).barcode(argCaptor.capture());
+		System.out.println(argCaptor.getValue());
 		
 		//verify display was called with "123"
-		verify(mockDisplay).showLine("123");
+		verify(mockDisplay).showLine(argCaptor.getValue());
+		
 		//verify display.showLine called with Milk, 3.99
 		verify(mockDisplay).showLine("Milk, 3.99");
+		
+		sale.scan("124");
 		
 		//verify that Storage.barcode was called by the sale object
 		verify(mockStorage).barcode("123");
