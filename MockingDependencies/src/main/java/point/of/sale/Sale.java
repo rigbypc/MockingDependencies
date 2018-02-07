2937277cccd3942830c2986e7157b9ac7f885eac
Peter C Rigby <rigbypc@gmail.com>
2018-02-07 14:21:36 -0500

    Add Interac to Sale

--- MockingDependencies/src/main/java/point/of/sale/Sale.java
+++ MockingDependencies/src/main/java/point/of/sale/Sale.java
@@ -1,13 +1,22 @@
 package point.of.sale;
 
+import java.util.ArrayList;
+
 public class Sale {
 
 	Storage storage;
 	Display display;
+	Interac interac;
+	ArrayList<String> items = new ArrayList<>();
 	
 	public Sale(Display display, Storage storage) {
 		this.display = display;
 		this.storage = storage;
+		this.interac = new Interac(12);
+	}
+	
+	public void completePurchase() {
+		interac.pay(items);
 	}
 	
 	public void scan(String barcode) {
@@ -15,7 +24,9 @@ public class Sale {
 		display.showLine(barcode);
 		
 		//lookup barcode in postgres and get item
-		display.showLine(storage.barcode(barcode));
+		String item = storage.barcode(barcode);
+		display.showLine(item);
+		items.add(item);
 		
 	}
 
