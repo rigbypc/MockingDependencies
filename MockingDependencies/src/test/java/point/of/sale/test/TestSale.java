3e1c95b79b9bc454c155c04ad3db609653239864
Peter C Rigby <rigbypc@gmail.com>
2018-03-12 15:44:44 -0400

    when and verify with the new mocks

--- MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
+++ MockingDependencies/src/test/java/point/of/sale/test/TestSale.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.*;
+
 import point.of.sale.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -30,7 +32,15 @@ public class TestSale {
 	
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		//setup the storage mock to return milk
+		when(mockStorage.barcode("123")).thenReturn("Milk 3.99");
+		//call the class under test
+		sale.scan("123");
+		
+		//verify that storage mock is called
+		verify(mockStorage).barcode("123");
+		//verify that display is called
+		verify(mockDisplay).showLine("Milk 3.99");
 	}
-
+	
 }
