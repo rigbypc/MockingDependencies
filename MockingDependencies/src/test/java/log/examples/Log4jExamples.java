c9bae940dee2a3e53d3c1ccf8ea1a584126156d0
Peter C Rigby <rigbypc@gmail.com>
2018-03-28 10:59:26 -0400

    Basic logging setup

---
+++ MockingDependencies/src/test/java/log/examples/Log4jExamples.java
@@ -0,0 +1,21 @@
+package log.examples;
+
+import static org.junit.Assert.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+
+public class Log4jExamples {
+
+	private static Logger logger = LogManager.getRootLogger();
+	
+	@Test
+	public void test() {
+		logger.debug("Debug log");
+		logger.info("Info log");
+		logger.error("Error log");
+	}
+
+}
