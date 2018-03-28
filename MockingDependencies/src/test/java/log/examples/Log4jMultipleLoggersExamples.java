408e1e7879cf2235589c1887eb2a4d779259af76
Peter C Rigby <rigbypc@gmail.com>
2018-03-28 12:02:09 -0400

    Remove from test the other log types so we have a starting example.

--- MockingDependencies/src/test/java/log/examples/Log4jMultipleLoggersExamples.java
+++ MockingDependencies/src/test/java/log/examples/Log4jMultipleLoggersExamples.java
@@ -7,15 +7,36 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 
-public class Log4jExamples {
+public class Log4jMultipleLoggersExamples {
 
-	private static Logger logger = LogManager.getLogger();
+	private static Logger rootLog = LogManager.getLogger();
+	private static Logger consoleLog = LogManager.getLogger("analytics.console");
+	private static Logger fileLog = LogManager.getLogger("analytics");
 	
 	@Test
-	public void test() {
-		logger.debug("Debug log");
-		logger.info("Info log");
-		logger.error("Error log");
+	public void testRoot() {
+		System.out.println("Root logger");
+		rootLog.debug("Debug log");
+		rootLog.info("Info log");
+		rootLog.error("Error log");
+				
+	}
+	
+	//@Test
+	public void testFile() {
+		System.out.println("File Logger");
+		fileLog.debug("Debug log");
+		fileLog.info("Info log");
+		fileLog.error("Error log");
+				
+	}
+	
+	//@Test
+	public void testConsole() {
+		System.out.println("Console Logger");
+		consoleLog.debug("Debug log");
+		consoleLog.info("Info log");
+		consoleLog.error("Error log");
 	}
 
 }
